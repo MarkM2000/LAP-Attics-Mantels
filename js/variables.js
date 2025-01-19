c9b33b55d07253a63410f6e6055e5b6a1f48bada
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2025-01-19 16:35:38 -0500

    Create variables.js

---
+++ js/variables.js
@@ -0,0 +1,36 @@
+// Adding variables for checkboxes 
+/// Attic
+var attic = L.geoJson(attic, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+            color: '#1f78b4',
+            weight: 1,
+            fillColor: '#0D98BA',
+            fillOpacity: .8,
+            radius: 10
+        });
+    },
+    onEachFeature: function (feature, layer) {
+        const props = feature.properties
+        const popup = `
+                <b>${props.informant}</b>
+                <br>Attic type: ${props.attic}<br>
+            `
+        layer.bindTooltip(popup, {
+            className: 'tool-informant'
+        });
+
+        layer.on('mouseover', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#0D98BA'
+            });
+        });
+        layer.on('mouseout', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#0D98BA'
+            });
+        });
+    }
+})
