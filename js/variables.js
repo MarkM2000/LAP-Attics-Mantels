d5ad9f7004e943f4a0fe9c6288231963c5aa4aa4
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2025-01-20 14:54:25 -0500

    add loft variable

--- js/variables.js
+++ js/variables.js
@@ -68,3 +68,38 @@ var attic = L.geoJson(attic, {
         });
     }
 })
+
+var loft = L.geoJson(loft, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+            color: '#1f78b4',
+            weight: 1,
+            fillColor: '#0D98BA',
+            fillOpacity: .8,
+            radius: 10
+        });
+    },
+    onEachFeature: function (feature, layer) {
+        const props = feature.properties
+        const popup = `
+                <b>${props.informant}</b>
+                <br>Attic type: ${props.attic_type}<br>
+            `
+        layer.bindTooltip(popup, {
+            className: 'tool-informant'
+        });
+
+        layer.on('mouseover', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#0D98BA'
+            });
+        });
+        layer.on('mouseout', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#0D98BA'
+            });
+        });
+    }
+})
