1afff50cef5c92f958258ffc3396e54f73c1e94b
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2025-01-21 18:23:07 -0500

    add mantelpiece layer

--- js/variables.js
+++ js/variables.js
@@ -174,3 +174,38 @@ var mantelboard = L.geoJson(mantelboard, {
         });
     }
 })
+
+var mantelpiece = L.geoJson(mantelpiece, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+            color: '#1f78b4',
+            weight: 1,
+            fillColor: '#0D98BA',
+            fillOpacity: .8,
+            radius: 10
+        });
+    },
+    onEachFeature: function (feature, layer) {
+        const props = feature.properties
+        const popup = `
+                <b>${props.informant}</b>
+                <br>Attic type: ${props.mantel}<br>
+            `
+        layer.bindTooltip(popup, {
+            className: 'tool-informant'
+        });
+
+        layer.on('mouseover', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#0D98BA'
+            });
+        });
+        layer.on('mouseout', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#0D98BA'
+            });
+        });
+    }
+})
