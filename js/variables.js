9e4a761381bb2b16a412604816ca0491590de958
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2025-01-22 17:04:00 -0500

    rmv dup

--- js/variables.js
+++ js/variables.js
@@ -211,41 +211,6 @@ var mantelpiece = L.geoJson(mantelpiece, {
     }
 })
 
-var mantelboard = L.geoJson(mantel, {
-    pointToLayer: function (feature, latlng) {
-        return L.circleMarker(latlng, {
-            color: '#FFFFFF',
-            weight: 1,
-            fillColor: '#ffa9d0',
-            fillOpacity: .8,
-            radius: 10
-        });
-    },
-    onEachFeature: function (feature, layer) {
-        const props = feature.properties
-        const popup = `
-					<b>${props.Informant}</b>
-					<br>Attic type: ${props.mantel}<br>
-				`
-        layer.bindTooltip(popup, {
-            className: 'tool-informant'
-        });
-
-        layer.on('mouseover', function () {
-            // code goes in here
-            layer.setStyle({
-                fillColor: '#ffa9d0'
-            });
-        });
-        layer.on('mouseout', function () {
-            // code goes in here
-            layer.setStyle({
-                fillColor: '#ffa9d0'
-            });
-        });
-    }
-})
-
 var mantelshelf = L.geoJson(mantelshelf, {
     pointToLayer: function (feature, latlng) {
         return L.circleMarker(latlng, {
