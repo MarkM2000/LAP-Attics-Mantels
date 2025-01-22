1d823e25ce800a3ca7e9a1ece178eb3d086c5dda
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2025-01-22 15:53:35 -0500

    readd

--- js/variables.js
+++ js/variables.js
@@ -209,3 +209,38 @@ var mantelpiece = L.geoJson(mantelpiece, {
         });
     }
 })
+
+var mantelshelf = L.geoJson(mantelshelf, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+            color: '#1f78b4',
+            weight: 1,
+            fillColor: '#0D98BA',
+            fillOpacity: .8,
+            radius: 10
+        });
+    },
+    onEachFeature: function (feature, layer) {
+        const props = feature.properties
+        const popup = `
+                <b>${props.informant}</b>
+                <br>Attic type: ${props.mantel}<br>
+            `
+        layer.bindTooltip(popup, {
+            className: 'tool-informant'
+        });
+
+        layer.on('mouseover', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#0D98BA'
+            });
+        });
+        layer.on('mouseout', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#0D98BA'
+            });
+        });
+    }
+})
