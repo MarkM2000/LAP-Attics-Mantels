2d54b467535076e19c964a54ff9387b1ccd09664
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2025-01-22 16:44:34 -0500

    fix attic variables

--- js/variables.js
+++ js/variables.js
@@ -1,5 +1,5 @@
 // Adding variables for checkboxes 
-/// Attic variable
+/// attic variables 
 var attic = L.geoJson(attic, {
     pointToLayer: function (feature, latlng) {
         return L.circleMarker(latlng, {
@@ -13,7 +13,6 @@ var attic = L.geoJson(attic, {
     onEachFeature: function (feature, layer) {
         const props = feature.properties
         const popup = `
-					<b>${props.Informant}</b>
 					<br>Gender: ${props.attic_type}<br>
 				`
         layer.bindTooltip(popup, {
@@ -35,12 +34,12 @@ var attic = L.geoJson(attic, {
     }
 })
 
-var loft = L.geoJson(attic, {
+var garret = L.geoJson(garret, {
     pointToLayer: function (feature, latlng) {
         return L.circleMarker(latlng, {
             color: '#FFFFFF',
             weight: 1,
-            fillColor: '#ffa9d0',
+            fillColor: '#2986cc',
             fillOpacity: .8,
             radius: 10
         });
@@ -58,24 +57,24 @@ var loft = L.geoJson(attic, {
         layer.on('mouseover', function () {
             // code goes in here
             layer.setStyle({
-                fillColor: '#ffa9d0'
+                fillColor: '#2986cc'
             });
         });
         layer.on('mouseout', function () {
             // code goes in here
             layer.setStyle({
-                fillColor: '#ffa9d0'
+                fillColor: '#2986cc'
             });
         });
     }
 })
 
-var garret = L.geoJson(attic, {
+var loft = L.geoJson(loft, {
     pointToLayer: function (feature, latlng) {
         return L.circleMarker(latlng, {
             color: '#FFFFFF',
             weight: 1,
-            fillColor: '#ffa9d0',
+            fillColor: '#2986cc',
             fillOpacity: .8,
             radius: 10
         });
@@ -93,155 +92,15 @@ var garret = L.geoJson(attic, {
         layer.on('mouseover', function () {
             // code goes in here
             layer.setStyle({
-                fillColor: '#ffa9d0'
+                fillColor: '#2986cc'
             });
         });
         layer.on('mouseout', function () {
             // code goes in here
             layer.setStyle({
-                fillColor: '#ffa9d0'
+                fillColor: '#2986cc'
             });
         });
     }
 })
 
-/// Mantel
-var mantel = L.geoJson(mantel, {
-    pointToLayer: function (feature, latlng) {
-        return L.circleMarker(latlng, {
-            color: '#FFFFFF',
-            weight: 1,
-            fillColor: '#ffa9d0',
-            fillOpacity: .8,
-            radius: 10
-        });
-    },
-    onEachFeature: function (feature, layer) {
-        const props = feature.properties
-        const popup = `
-					<b>${props.Informant}</b>
-					<br>Gender: ${props.mantel}<br>
-				`
-        layer.bindTooltip(popup, {
-            className: 'tool-informant'
-        });
-
-        layer.on('mouseover', function () {
-            // code goes in here
-            layer.setStyle({
-                fillColor: '#ffa9d0'
-            });
-        });
-        layer.on('mouseout', function () {
-            // code goes in here
-            layer.setStyle({
-                fillColor: '#ffa9d0'
-            });
-        });
-    }
-})
-
-var mantelboard = L.geoJson(mantelboard, {
-    pointToLayer: function (feature, latlng) {
-        return L.circleMarker(latlng, {
-            color: '#FFFFFF',
-            weight: 1,
-            fillColor: '#ffa9d0',
-            fillOpacity: .8,
-            radius: 10
-        });
-    },
-    onEachFeature: function (feature, layer) {
-        const props = feature.properties
-        const popup = `
-					<b>${props.Informant}</b>
-					<br>Gender: ${props.mantel}<br>
-				`
-        layer.bindTooltip(popup, {
-            className: 'tool-informant'
-        });
-
-        layer.on('mouseover', function () {
-            // code goes in here
-            layer.setStyle({
-                fillColor: '#ffa9d0'
-            });
-        });
-        layer.on('mouseout', function () {
-            // code goes in here
-            layer.setStyle({
-                fillColor: '#ffa9d0'
-            });
-        });
-    }
-})
-
-var mantelpiece = L.geoJson(mantelpiece, {
-    pointToLayer: function (feature, latlng) {
-        return L.circleMarker(latlng, {
-            color: '#FFFFFF',
-            weight: 1,
-            fillColor: '#ffa9d0',
-            fillOpacity: .8,
-            radius: 10
-        });
-    },
-    onEachFeature: function (feature, layer) {
-        const props = feature.properties
-        const popup = `
-					<b>${props.Informant}</b>
-					<br>Gender: ${props.mantel}<br>
-				`
-        layer.bindTooltip(popup, {
-            className: 'tool-informant'
-        });
-
-        layer.on('mouseover', function () {
-            // code goes in here
-            layer.setStyle({
-                fillColor: '#ffa9d0'
-            });
-        });
-        layer.on('mouseout', function () {
-            // code goes in here
-            layer.setStyle({
-                fillColor: '#ffa9d0'
-            });
-        });
-    }
-})
-
-var mantelshelf = L.geoJson(mantelshelf, {
-    pointToLayer: function (feature, latlng) {
-        return L.circleMarker(latlng, {
-            color: '#FFFFFF',
-            weight: 1,
-            fillColor: '#ffa9d0',
-            fillOpacity: .8,
-            radius: 10
-        });
-    },
-    onEachFeature: function (feature, layer) {
-        const props = feature.properties
-        const popup = `
-					<b>${props.Informant}</b>
-					<br>Gender: ${props.mantel}<br>
-				`
-        layer.bindTooltip(popup, {
-            className: 'tool-informant'
-        });
-
-        layer.on('mouseover', function () {
-            // code goes in here
-            layer.setStyle({
-                fillColor: '#ffa9d0'
-            });
-        });
-        layer.on('mouseout', function () {
-            // code goes in here
-            layer.setStyle({
-                fillColor: '#ffa9d0'
-            });
-        });
-    }
-})
