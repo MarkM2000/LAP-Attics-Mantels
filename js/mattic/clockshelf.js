c18287bcc29eb496bd6be174a25b22ce8f9c24f5
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2025-01-23 17:08:08 -0500

    Create clockshelf.js

---
+++ js/mattic/clockshelf.js
@@ -0,0 +1,725 @@
+var clockshelf = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NY21",
+          "mantel": "clock_shelf",
+          "Project": "LAMSAS",
+          "Sex": "Male",
+          "Age": 73,
+          "Ethnicity ": "White",
+          "Education level": "At least some high school",
+          "Occupation": "Farmer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -74.74611,
+            42.85917
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NY23A",
+          "mantel": "clock_shelf",
+          "Project": "LAMSAS",
+          "Sex": "Male",
+          "Age": 84,
+          "Ethnicity ": "White",
+          "Education level": "Between 1 and 8 years",
+          "Occupation": "Manager, official, or proprietor"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -73.83278,
+            43.24445
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NY25A",
+          "mantel": "clock_shelf",
+          "Project": "LAMSAS",
+          "Sex": "Male",
+          "Age": 75,
+          "Ethnicity ": "White",
+          "Education level": "Between 1 and 8 years",
+          "Occupation": "Farmer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -73.96167,
+            43.61833
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NY29A",
+          "mantel": "clock_shelf",
+          "Project": "LAMSAS",
+          "Sex": "Male",
+          "Age": 84,
+          "Ethnicity ": "White",
+          "Education level": "Between 1 and 8 years",
+          "Occupation": "Farmer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -74.03389,
+            44.85778
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NY29C",
+          "mantel": "clock_shelf",
+          "Project": "LAMSAS",
+          "Sex": "Male",
+          "Age": 70,
+          "Ethnicity ": "White",
+          "Education level": "At least some high school",
+          "Occupation": "Manager, official, or proprietor"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -74.29166,
+            44.84167
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NY30A",
+          "mantel": "clock_shelf",
+          "Project": "LAMSAS",
+          "Sex": "Male",
+          "Age": 82,
+          "Ethnicity ": "White",
+          "Education level": "Between 1 and 8 years",
+          "Occupation": "Farmer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -74.88805,
+            44.51056
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NY30D",
+          "mantel": "clock_shelf",
+          "Project": "LAMSAS",
+          "Sex": "Male",
+          "Age": 77,
+          "Ethnicity ": "White",
+          "Education level": "Between 1 and 8 years",
+          "Occupation": "Farmer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -74.70834,
+            44.7
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NY30E",
+          "mantel": "clock_shelf",
+          "Project": "LAMSAS",
+          "Sex": "Female",
+          "Age": 75,
+          "Ethnicity ": "White",
+          "Education level": "At least some high school",
+          "Occupation": "Keeping house"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -74.68195,
+            44.69083
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NY31B",
+          "mantel": "clock_shelf",
+          "Project": "LAMSAS",
+          "Sex": "Male",
+          "Age": 76,
+          "Ethnicity ": "White",
+          "Education level": "Between 1 and 8 years",
+          "Occupation": "Farmer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -75.7975,
+            44.21528
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NY32A",
+          "mantel": "clock_shelf",
+          "Project": "LAMSAS",
+          "Sex": "Male",
+          "Age": 77,
+          "Ethnicity ": "White",
+          "Education level": "Between 1 and 8 years",
+          "Occupation": "Farmer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -75.42889,
+            43.56667
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NY43A",
+          "mantel": "clock_shelf",
+          "Project": "LAMSAS",
+          "Sex": "Male",
+          "Age": 80,
+          "Ethnicity ": "White",
+          "Education level": "Unknown",
+          "Occupation": "Farmer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -75.96806,
+            42.32889
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NY43B",
+          "mantel": "clock_shelf",
+          "Project": "LAMSAS",
+          "Sex": "Male",
+          "Age": 41,
+          "Ethnicity ": "White",
+          "Education level": "At least some high school",
+          "Occupation": "Farmer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -76.06194,
+            42.35833
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NY46A",
+          "mantel": "clock_shelf",
+          "Project": "LAMSAS",
+          "Sex": "Male",
+          "Age": 82,
+          "Ethnicity ": "White",
+          "Education level": "Between 1 and 8 years",
+          "Occupation": "Craftsman or foreman"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -77.04945,
+            42.30556
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NY51A",
+          "mantel": "clock_shelf",
+          "Project": "LAMSAS",
+          "Sex": "Male",
+          "Age": 80,
+          "Ethnicity ": "White",
+          "Education level": "Between 1 and 8 years",
+          "Occupation": "Farmer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -77.53472,
+            42.80972
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NY51C",
+          "mantel": "clock_shelf",
+          "Project": "LAMSAS",
+          "Sex": "Male",
+          "Age": 75,
+          "Ethnicity ": "White",
+          "Education level": "Between 1 and 8 years",
+          "Occupation": "Farmer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -77.40278,
+            42.61528
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NY51D",
+          "mantel": "clock_shelf",
+          "Project": "LAMSAS",
+          "Sex": "Female",
+          "Age": 70,
+          "Ethnicity ": "White",
+          "Education level": "At least some high school",
+          "Occupation": "Keeping house"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -77.40833,
+            42.60695
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NY52A",
+          "mantel": "clock_shelf",
+          "Project": "LAMSAS",
+          "Sex": "Female",
+          "Age": 49,
+          "Ethnicity ": "White",
+          "Education level": "At least some high school",
+          "Occupation": "Keeping house"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -77.29222,
+            42.61167
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NY53A",
+          "mantel": "clock_shelf",
+          "Project": "LAMSAS",
+          "Sex": "Male",
+          "Age": 85,
+          "Ethnicity ": "White",
+          "Education level": "Between 1 and 8 years",
+          "Occupation": "Farmer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -77.35694,
+            42.30305
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NY55B",
+          "mantel": "clock_shelf",
+          "Project": "LAMSAS",
+          "Sex": "Female",
+          "Age": 85,
+          "Ethnicity ": "White",
+          "Education level": "At least some high school",
+          "Occupation": "Keeping house"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -78.30917,
+            42.35917
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NY57C!",
+          "mantel": "clock_shelf",
+          "Project": "LAMSAS",
+          "Sex": "Female",
+          "Age": 83,
+          "Ethnicity ": "White",
+          "Education level": "At least some college or beyond",
+          "Occupation": "Teacher"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -77.61166,
+            42.90472
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NY58C",
+          "mantel": "clock_shelf",
+          "Project": "LAMSAS",
+          "Sex": "Male",
+          "Age": 63,
+          "Ethnicity ": "White",
+          "Education level": "At least some college or beyond",
+          "Occupation": "Manager, official, or proprietor"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -77.61584,
+            43.15472
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NY59A",
+          "mantel": "clock_shelf",
+          "Project": "LAMSAS",
+          "Sex": "Male",
+          "Age": 71,
+          "Ethnicity ": "White",
+          "Education level": "At least some high school",
+          "Occupation": "Farmer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -77.9425,
+            43.08528
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NY60A",
+          "mantel": "clock_shelf",
+          "Project": "LAMSAS",
+          "Sex": "Male",
+          "Age": 87,
+          "Ethnicity ": "White",
+          "Education level": "Between 1 and 8 years",
+          "Occupation": "Farmer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -78.38917,
+            43.32667
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NY60A",
+          "mantel": "clock_shelf",
+          "Project": "LAMSAS",
+          "Sex": "Male",
+          "Age": 87,
+          "Ethnicity ": "White",
+          "Education level": "Between 1 and 8 years",
+          "Occupation": "Farmer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -78.38917,
+            43.32667
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NY63A",
+          "mantel": "clock_shelf",
+          "Project": "LAMSAS",
+          "Sex": "Male",
+          "Age": 70,
+          "Ethnicity ": "White",
+          "Education level": "At least some high school",
+          "Occupation": "Farmer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -78.60833,
+            42.20083
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NJ14A",
+          "mantel": "clock_shelf",
+          "Project": "LAMSAS",
+          "Sex": "Male",
+          "Age": 78,
+          "Ethnicity ": "White",
+          "Education level": "At least some high school",
+          "Occupation": "Farmer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -74.96083,
+            40.98278
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "PA11B",
+          "mantel": "clock_shelf",
+          "Project": "LAMSAS",
+          "Sex": "Male",
+          "Age": 37,
+          "Ethnicity ": "White",
+          "Education level": "At least some high school",
+          "Occupation": "Farmer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -76.79778,
+            40.89167
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NC10A",
+          "mantel": "clock_shelf",
+          "Project": "LAMSAS",
+          "Sex": "Male",
+          "Age": 76,
+          "Ethnicity ": "White",
+          "Education level": "Unknown",
+          "Occupation": "Fisher"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -76.2525,
+            35.9175
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NC20C",
+          "mantel": "clock_shelf",
+          "Project": "LAMSAS",
+          "Sex": "Female",
+          "Age": 42,
+          "Ethnicity ": "White",
+          "Education level": "At least some high school",
+          "Occupation": "Keeping house"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -76.30917,
+            34.99222
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "IL42B",
+          "mantel": "clock_shelf",
+          "Project": "LANCS",
+          "Sex": "Male",
+          "Age": 60,
+          "Ethnicity ": "White",
+          "Education level": "At least some high school",
+          "Occupation": "Farmer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -87.6961,
+            39.6111
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "MI7A",
+          "mantel": "clock_shelf",
+          "Project": "LANCS",
+          "Sex": "Male",
+          "Age": 56,
+          "Ethnicity ": "White",
+          "Education level": "At least some high school",
+          "Occupation": "Farmer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -84.9769,
+            45.6419
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "MI9A",
+          "mantel": "clock_shelf",
+          "Project": "LANCS",
+          "Sex": "Male",
+          "Age": 87,
+          "Ethnicity ": "White",
+          "Education level": "At least some high school",
+          "Occupation": "Manager, official, or proprietor"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -83.4328,
+            45.0617
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "MI10B",
+          "mantel": "clock_shelf",
+          "Project": "LANCS",
+          "Sex": "Male",
+          "Age": 80,
+          "Ethnicity ": "White",
+          "Education level": "Between 1 and 8 yrs.",
+          "Occupation": "Farmer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -86.3174,
+            43.8654
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "MI14B",
+          "mantel": "clock_shelf",
+          "Project": "LANCS",
+          "Sex": "Male",
+          "Age": 68,
+          "Ethnicity ": "White",
+          "Education level": "Between 1 and 8 yrs.",
+          "Occupation": "Farmer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -84.7942,
+            43.6917
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "VT244",
+          "mantel": "clock_shelf",
+          "Project": "LANE",
+          "Sex": "Male",
+          "Age": 77,
+          "Ethnicity ": "White",
+          "Education level": "Between 1 and 8 years",
+          "Occupation": "Farmer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -72.98,
+            44.3038
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "TN318",
+          "mantel": "clock_shelf ",
+          "Project": "LAGS",
+          "Sex": "Male",
+          "Age": 72,
+          "Ethnicity ": "White",
+          "Education level": "At least some high school",
+          "Occupation": "Farmer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -87.6103,
+            35.9087
+          ]
+        }
+      }
+    ]
+  }
