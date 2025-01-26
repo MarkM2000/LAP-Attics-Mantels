233c7cf80b034bd03bb6809af283dd64f5154c9e
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2025-01-25 19:44:48 -0500

    add fry_pan variable

--- js/pan-fence-variables.js
+++ js/pan-fence-variables.js
@@ -34,3 +34,38 @@ var frying_pan = L.geoJson(frying_pan, {
         });
     }
 })
+
+var fry_pan = L.geoJson(fry_pan, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+            color: '#FFFFFF',
+            weight: 1,
+            fillColor: '#5a5247',
+            fillOpacity: .8,
+            radius: 10
+        });
+    },
+    onEachFeature: function (feature, layer) {
+        const props = feature.properties
+        const popup = `
+					<b>${props.Informant}</b>
+					<br>Pan type: ${props.frying_pan}<br>
+				`
+        layer.bindTooltip(popup, {
+            className: 'tool-informant'
+        });
+
+        layer.on('mouseover', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#5a5247'
+            });
+        });
+        layer.on('mouseout', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#5a5247'
+            });
+        });
+    }
+})
