31bac144b6968516491121f522b0c6258d57ae9c
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2025-01-25 19:41:17 -0500

    Update pan-fence-variables.js

--- js/pan-fence-variables.js
+++ js/pan-fence-variables.js
@@ -34,38 +34,3 @@ var frying_pan = L.geoJson(frying_pan, {
         });
     }
 })
-
-var spider = L.geoJson(spider, {
-    pointToLayer: function (feature, latlng) {
-        return L.circleMarker(latlng, {
-            color: '#FFFFFF',
-            weight: 1,
-            fillColor: '#866965',
-            fillOpacity: .8,
-            radius: 10
-        });
-    },
-    onEachFeature: function (feature, layer) {
-        const props = feature.properties
-        const popup = `
-					<b>${props.Informant}</b>
-					<br>Attic type: ${props.frying_pan}<br>
-				`
-        layer.bindTooltip(popup, {
-            className: 'tool-informant'
-        });
-
-        layer.on('mouseover', function () {
-            // code goes in here
-            layer.setStyle({
-                fillColor: '#866965'
-            });
-        });
-        layer.on('mouseout', function () {
-            // code goes in here
-            layer.setStyle({
-                fillColor: '#866965'
-            });
-        });
-    }
-})
