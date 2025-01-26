3d655f76b1c1e123ad66689020d546c807ae65d5
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2025-01-25 19:25:27 -0500

    add spider variable

--- js/pan-fence-variables.js
+++ js/pan-fence-variables.js
@@ -14,7 +14,7 @@ var frying_pan = L.geoJson(frying_pan, {
         const props = feature.properties
         const popup = `
 					<b>${props.informant}</b>
-					<br>Attic type: ${props.frying_pan}<br>
+					<br>Pan type: ${props.frying_pan}<br>
 				`
         layer.bindTooltip(popup, {
             className: 'tool-informant'
@@ -49,7 +49,7 @@ var frypan = L.geoJson(frypan, {
         const props = feature.properties
         const popup = `
 					<b>${props.informant}</b>
-					<br>Attic type: ${props.frying_pan}<br>
+					<br>Pan type: ${props.frying_pan}<br>
 				`
         layer.bindTooltip(popup, {
             className: 'tool-informant'
@@ -65,6 +65,41 @@ var frypan = L.geoJson(frypan, {
             // code goes in here
             layer.setStyle({
                 fillColor: '#5a5247'
+            });
+        });
+    }
+})
+
+var spider = L.geoJson(spider, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+            color: '#FFFFFF',
+            weight: 1,
+            fillColor: '#866965',
+            fillOpacity: .8,
+            radius: 10
+        });
+    },
+    onEachFeature: function (feature, layer) {
+        const props = feature.properties
+        const popup = `
+					<b>${props.Informant}</b>
+					<br>Attic type: ${props.frying_pan}<br>
+				`
+        layer.bindTooltip(popup, {
+            className: 'tool-informant'
+        });
+
+        layer.on('mouseover', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#866965'
+            });
+        });
+        layer.on('mouseout', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#866965'
             });
         });
     }
