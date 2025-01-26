86747e2cdc242f40f76ff0d04389e51e07c43e02
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2025-01-25 19:31:16 -0500

    rmv frypan stuff

--- js/pan-fence-variables.js
+++ js/pan-fence-variables.js
@@ -35,41 +35,6 @@ var frying_pan = L.geoJson(frying_pan, {
     }
 })
 
-var frypan = L.geoJson(frypan, {
-    pointToLayer: function (feature, latlng) {
-        return L.circleMarker(latlng, {
-            color: '#FFFFFF',
-            weight: 1,
-            fillColor: '#5a5247',
-            fillOpacity: .8,
-            radius: 10
-        });
-    },
-    onEachFeature: function (feature, layer) {
-        const props = feature.properties
-        const popup = `
-					<b>${props.informant}</b>
-					<br>Pan type: ${props.frying_pan}<br>
-				`
-        layer.bindTooltip(popup, {
-            className: 'tool-informant'
-        });
-
-        layer.on('mouseover', function () {
-            // code goes in here
-            layer.setStyle({
-                fillColor: '#5a5247'
-            });
-        });
-        layer.on('mouseout', function () {
-            // code goes in here
-            layer.setStyle({
-                fillColor: '#5a5247'
-            });
-        });
-    }
-})
-
 var spider = L.geoJson(spider, {
     pointToLayer: function (feature, latlng) {
         return L.circleMarker(latlng, {
