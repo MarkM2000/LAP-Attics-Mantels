3c19513b3c8116490e52f6247382453e4f3e5a09
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2025-01-27 20:18:33 -0500

    add var skillet

--- js/pan-fence-variables.js
+++ js/pan-fence-variables.js
@@ -139,3 +139,38 @@ var iron_skillet = L.geoJson(iron_skillet, {
         });
     }
 })
+
+var skillet = L.geoJson(skillet, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+            color: '#FFFFFF',
+            weight: 1,
+            fillColor: '#7A6A58',
+            fillOpacity: .8,
+            radius: 10
+        });
+    },
+    onEachFeature: function (feature, layer) {
+        const props = feature.properties
+        const popup = `
+					<b>${props.Informant}</b>
+					<br>Pan type: ${props.frying_pan}<br>
+				`
+        layer.bindTooltip(popup, {
+            className: 'tool-informant'
+        });
+
+        layer.on('mouseover', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#7A6A58'
+            });
+        });
+        layer.on('mouseout', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#7A6A58'
+            });
+        });
+    }
+})
