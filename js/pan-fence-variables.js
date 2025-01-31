e137f96c296c939bb787d155c63bbd7d090038a3
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2025-01-31 17:34:59 -0500

    add picket variable

--- js/pan-fence-variables.js
+++ js/pan-fence-variables.js
@@ -385,3 +385,38 @@ var picket_fences = L.geoJson(picket_fences, {
         });
     }
 })
+
+var picket = L.geoJson(picket, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+            color: '#FFFFFF',
+            weight: 1,
+            fillColor: '#7A6A58',
+            fillOpacity: .8,
+            radius: 5
+        });
+    },
+    onEachFeature: function (feature, layer) {
+        const props = feature.properties
+        const popup = `
+					<b>${props.Informant}</b>
+					<br>Pan type: ${props.picket_fence}<br>
+				`
+        layer.bindTooltip(popup, {
+            className: 'tool-informant'
+        });
+
+        layer.on('mouseover', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#7A6A58'
+            });
+        });
+        layer.on('mouseout', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#7A6A58'
+            });
+        });
+    }
+})
