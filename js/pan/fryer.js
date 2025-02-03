e81658021367526b95b2b6da95c3b22fc72da088
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2025-02-03 17:35:04 -0500

    Create fryer.js

---
+++ js/pan/fryer.js
@@ -0,0 +1,745 @@
+var fryer = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NY19",
+          "Project": "LAMSAS",
+          "Sex": "Male",
+          "Age": 52,
+          "Ethnicity ": "White",
+          "Education_level": "At least some high school",
+          "Occupation": "Farmer",
+          "frying_pan": "fryer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -74.13416,
+            42.76194
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NY58F!",
+          "Project": "LAMSAS",
+          "Sex": "Female",
+          "Age": 62,
+          "Ethnicity ": "White",
+          "Education_level": "At least some college or beyond",
+          "Occupation": "Keeping house",
+          "frying_pan": "fryer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -77.65,
+            43.16667
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NY58F!",
+          "Project": "LAMSAS",
+          "Sex": "Female",
+          "Age": 62,
+          "Ethnicity ": "White",
+          "Education_level": "At least some college or beyond",
+          "Occupation": "Keeping house",
+          "frying_pan": "fryer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -77.65,
+            43.16667
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "VA70A",
+          "Project": "LAMSAS",
+          "Sex": "Female",
+          "Age": 61,
+          "Ethnicity ": "White",
+          "Education_level": "At least some high school",
+          "Occupation": "Keeping house",
+          "frying_pan": "fryer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -80.0075,
+            36.61972
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "VA70A",
+          "Project": "LAMSAS",
+          "Sex": "Female",
+          "Age": 61,
+          "Ethnicity ": "White",
+          "Education_level": "At least some high school",
+          "Occupation": "Keeping house",
+          "frying_pan": "fryer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -80.0075,
+            36.61972
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "VA70A",
+          "Project": "LAMSAS",
+          "Sex": "Female",
+          "Age": 61,
+          "Ethnicity ": "White",
+          "Education_level": "At least some high school",
+          "Occupation": "Keeping house",
+          "frying_pan": "fryer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -80.0075,
+            36.61972
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NC5B",
+          "Project": "LAMSAS",
+          "Sex": "Female",
+          "Age": 44,
+          "Ethnicity ": "White",
+          "Education_level": "At least some college or beyond",
+          "Occupation": "Keeping house",
+          "frying_pan": "fryer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -76.59805,
+            36.36889
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NC52N",
+          "Project": "LAMSAS",
+          "Sex": "Female",
+          "Age": 90,
+          "Ethnicity ": "Black or African American",
+          "Education_level": "No formal schooling",
+          "Occupation": "Keeping house",
+          "frying_pan": "fryer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -79.77778,
+            35.47083
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "SC42A",
+          "Project": "LAMSAS",
+          "Sex": "Male",
+          "Age": 70,
+          "Ethnicity ": "White",
+          "Education_level": "Between 1 and 8 years",
+          "Occupation": "Farmer",
+          "frying_pan": "fryer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -82.45834,
+            34.875
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "SC42A",
+          "Project": "LAMSAS",
+          "Sex": "Male",
+          "Age": 70,
+          "Ethnicity ": "White",
+          "Education_level": "Between 1 and 8 years",
+          "Occupation": "Farmer",
+          "frying_pan": "fryer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -82.45834,
+            34.875
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "SC42A",
+          "Project": "LAMSAS",
+          "Sex": "Male",
+          "Age": 70,
+          "Ethnicity ": "White",
+          "Education_level": "Between 1 and 8 years",
+          "Occupation": "Farmer",
+          "frying_pan": "fryer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -82.45834,
+            34.875
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "SC42A",
+          "Project": "LAMSAS",
+          "Sex": "Male",
+          "Age": 70,
+          "Ethnicity ": "White",
+          "Education_level": "Between 1 and 8 years",
+          "Occupation": "Farmer",
+          "frying_pan": "fryer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -82.45834,
+            34.875
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "SC42A",
+          "Project": "LAMSAS",
+          "Sex": "Male",
+          "Age": 70,
+          "Ethnicity ": "White",
+          "Education_level": "Between 1 and 8 years",
+          "Occupation": "Farmer",
+          "frying_pan": "fryer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -82.45834,
+            34.875
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "SC42A",
+          "Project": "LAMSAS",
+          "Sex": "Male",
+          "Age": 70,
+          "Ethnicity ": "White",
+          "Education_level": "Between 1 and 8 years",
+          "Occupation": "Farmer",
+          "frying_pan": "fryer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -82.45834,
+            34.875
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "SC42A",
+          "Project": "LAMSAS",
+          "Sex": "Male",
+          "Age": 70,
+          "Ethnicity ": "White",
+          "Education_level": "Between 1 and 8 years",
+          "Occupation": "Farmer",
+          "frying_pan": "fryer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -82.45834,
+            34.875
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "SC42A",
+          "Project": "LAMSAS",
+          "Sex": "Male",
+          "Age": 70,
+          "Ethnicity ": "White",
+          "Education_level": "Between 1 and 8 years",
+          "Occupation": "Farmer",
+          "frying_pan": "fryer"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -82.45834,
+            34.875
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "GA084",
+          "Project": "LAGS",
+          "Sex": "Male",
+          "Age": 69,
+          "Ethnicity ": "Black or African American",
+          "Education_level": "Between 1 and 8 years",
+          "Occupation": "Laborer",
+          "frying_pan": "fryer "
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -84.0176,
+            33.6576
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "GA084",
+          "Project": "LAGS",
+          "Sex": "Male",
+          "Age": 69,
+          "Ethnicity ": "Black or African American",
+          "Education_level": "Between 1 and 8 years",
+          "Occupation": "Laborer",
+          "frying_pan": "fryer "
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -84.0176,
+            33.6576
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "GA089",
+          "Project": "LAGS",
+          "Sex": "Female",
+          "Age": 73,
+          "Ethnicity ": "White",
+          "Education_level": "At least some college or beyond",
+          "Occupation": "Keeping house",
+          "frying_pan": "fryer "
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -83.5712,
+            34.1186
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "GA095",
+          "Project": "LAGS",
+          "Sex": "Female",
+          "Age": 78,
+          "Ethnicity ": "White",
+          "Education_level": "At least some high school",
+          "Occupation": "Keeping house",
+          "frying_pan": "fryer "
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -84.1058,
+            33.7121
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "GA160",
+          "Project": "LAGS",
+          "Sex": "Male",
+          "Age": 67,
+          "Ethnicity ": "White",
+          "Education_level": "At least some high school",
+          "Occupation": "Farmer",
+          "frying_pan": "fryer "
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -81.6226,
+            32.1359
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "AL399",
+          "Project": "LAGS",
+          "Sex": "Male",
+          "Age": 71,
+          "Ethnicity ": "Black or African American",
+          "Education_level": "At least some high school",
+          "Occupation": "Manager, official or proprietor",
+          "frying_pan": "fryer "
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -86.5804,
+            32.802
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "AL399",
+          "Project": "LAGS",
+          "Sex": "Male",
+          "Age": 71,
+          "Ethnicity ": "Black or African American",
+          "Education_level": "At least some high school",
+          "Occupation": "Manager, official or proprietor",
+          "frying_pan": "fryer "
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -86.5804,
+            32.802
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "AL399",
+          "Project": "LAGS",
+          "Sex": "Male",
+          "Age": 71,
+          "Ethnicity ": "Black or African American",
+          "Education_level": "At least some high school",
+          "Occupation": "Manager, official or proprietor",
+          "frying_pan": "fryer "
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -86.5804,
+            32.802
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "AL410",
+          "Project": "LAGS",
+          "Sex": "Female",
+          "Age": 81,
+          "Ethnicity ": "Black or African American",
+          "Education_level": "Between 1 and 8 years",
+          "Occupation": "Manager, official or proprietor",
+          "frying_pan": "fryer "
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -85.4817,
+            32.6091
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "AL410",
+          "Project": "LAGS",
+          "Sex": "Female",
+          "Age": 81,
+          "Ethnicity ": "Black or African American",
+          "Education_level": "Between 1 and 8 years",
+          "Occupation": "Manager, official or proprietor",
+          "frying_pan": "fryer "
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -85.4817,
+            32.6091
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "AL438",
+          "Project": "LAGS",
+          "Sex": "Male",
+          "Age": 73,
+          "Ethnicity ": "Black or African American",
+          "Education_level": "Between 1 and 8 years",
+          "Occupation": "Farmer",
+          "frying_pan": "fryer "
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -86.6406,
+            31.8296
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "AL438",
+          "Project": "LAGS",
+          "Sex": "Male",
+          "Age": 73,
+          "Ethnicity ": "Black or African American",
+          "Education_level": "Between 1 and 8 years",
+          "Occupation": "Farmer",
+          "frying_pan": "fryer "
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -86.6406,
+            31.8296
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "AL441",
+          "Project": "LAGS",
+          "Sex": "Female",
+          "Age": 78,
+          "Ethnicity ": "Black or African American",
+          "Education_level": "Between 1 and 8 years",
+          "Occupation": "Farmer",
+          "frying_pan": "fryer "
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -85.931,
+            31.3871
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "AL390",
+          "Project": "LAGS",
+          "Sex": "Male",
+          "Age": 65,
+          "Ethnicity ": "White",
+          "Education_level": "At least some college or beyond",
+          "Occupation": "Manager, official or proprietor",
+          "frying_pan": "fryer "
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -85.3752,
+            33.1522
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "AL391",
+          "Project": "LAGS",
+          "Sex": "Male",
+          "Age": 27,
+          "Ethnicity ": "White",
+          "Education_level": "At least some college or beyond",
+          "Occupation": "Clerical or sales position",
+          "frying_pan": "fryer "
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -85.187,
+            32.8694
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "AL392",
+          "Project": "LAGS",
+          "Sex": "Male",
+          "Age": 72,
+          "Ethnicity ": "White",
+          "Education_level": "Between 1 and 8 years",
+          "Occupation": "Operative",
+          "frying_pan": "fryer "
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -85.9714,
+            32.9337
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "FL464",
+          "Project": "LAGS",
+          "Sex": "Female",
+          "Age": 76,
+          "Ethnicity ": "Black or African American",
+          "Education_level": "Between 1 and 8 years",
+          "Occupation": "Keeping house",
+          "frying_pan": "fryer "
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -86.5208,
+            30.8722
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "LA766",
+          "Project": "LAGS",
+          "Sex": "Female",
+          "Age": 72,
+          "Ethnicity ": "Black or African American",
+          "Education_level": "Between 1 and 8 years",
+          "Occupation": "Service worker",
+          "frying_pan": "fryer "
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -92.0853,
+            32.6958
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "FL467",
+          "Project": "LAGS",
+          "Sex": "Female",
+          "Age": 77,
+          "Ethnicity ": "White",
+          "Education_level": "Between 1 and 8 years",
+          "Occupation": "Keeping house",
+          "frying_pan": "fryer "
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -87.1512,
+            30.9513
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "MS582",
+          "Project": "LAGS",
+          "Sex": "Male",
+          "Age": 76,
+          "Ethnicity ": "White",
+          "Education_level": "Between 1 and 8 years",
+          "Occupation": "Farmer",
+          "frying_pan": "fryer "
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -88.7282,
+            32.038
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "RI18",
+          "Project": "LANE",
+          "Sex": "Female",
+          "Age": 66,
+          "Ethnicity ": "White",
+          "Education_level": "At least some high school",
+          "Occupation": "Keeping house",
+          "frying_pan": "fryer, skillet, spider "
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -71.58,
+            41.1697
+          ]
+        }
+      }
+    ]
+  }
